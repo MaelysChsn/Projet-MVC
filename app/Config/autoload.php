9f11553209d360cc838911432ebe038db696e264
Maélys Chassin <maelys.chassin05@gmail.com>
2021-12-07 23:23:21 +0100

    PDO fix

--- app/Config/autoload.php
+++ app/Config/autoload.php
@@ -4,7 +4,6 @@ spl_autoload_register(function($className){
 
     //className = Controller\PostController;
     //require = Controller\PostController.php;
-    var_dump($className);
     $className = str_replace("\\", "/", $className);
     require_once("$className.php");
 });
--- app/PDO/PDOFactory.php
+++ app/PDO/PDOFactory.php
@@ -8,9 +8,9 @@ class PDOFactory
     public static function getMysqlConnexion () {
         try
         {
-            //PDO est dans le namespace racine 
-            $db = new \PDO('mysql:host=localhost;dbname=db', 'user', 'password');
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //PDO est dans le namespace racine
+            $db = new \PDO('mysql:host=db;dbname=db', 'user', 'password');
+            $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
             return $db;
         } catch (Exception $e) {
             echo "Erreur : " . $e->getMessage() . "<br>";
